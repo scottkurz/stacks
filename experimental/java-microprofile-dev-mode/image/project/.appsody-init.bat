cf4e929db0d6425d8d6aa6a5e5e3c6c077b961ea
Andrew J. Mauer <ajmauer@gmail.com>
2019-11-18 17:46:07 -0500

    bin profile work

--- experimental/java-microprofile-dev-mode/image/project/.appsody-init.bat
+++ experimental/java-microprofile-dev-mode/image/project/.appsody-init.bat
@@ -1,1 +1,8 @@
+ECHO in the bat file
+IF EXIST "../migrationassets" (
+    ECHO found
+) ELSE (
+    ECHO not found
+	DIR
+)
 mvn install -Denforcer.skip=true
--- experimental/java-microprofile-dev-mode/image/project/.appsody-init.sh
+++ experimental/java-microprofile-dev-mode/image/project/.appsody-init.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 mvn install -Denforcer.skip=true
+
+FILE=./mavenassets
+if test -f "$FILE"; then
+    echo "$FILE exist"
+else echo "$FILE no exist"
+fi
